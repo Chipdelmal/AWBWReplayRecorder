7b98b8b4c3662da93b96a7f321b6c6d24d5df5ab
chipdelmal <chipdelmal@gmail.com>
2021-07-21 07:41:40 -0700

    Headless mode implemented

---
+++ constants.py
@@ -0,0 +1,9 @@
+
+
+# Path to chromedriver and output folder
+DRV_PTH = './chromedriver/chromedriver'
+OUT_PTH = '/home/chipdelmal/Documents/AWBW/'
+# Constants that probably don't require change
+SLEEP = 10
+HEADLESS = True
+BASE_URL = 'https://awbw.amarriner.com/2030.php?games_id=344511&ndx='
--- dev.py
+++ dev.py
@@ -1,47 +1,54 @@
 
+import os
+from sys import argv
 from time import sleep
 from os import path
 from PIL import Image
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+import constants as cst
 
-
-URL = 'https://awbw.amarriner.com/2030.php?games_id=347352&ndx='
-DRV = './chromedriver/chromedriver'
-OUT = './img/'
-SLP = 10
-(TRN, ZOM) = (74, ZOM)
+(ID, NAME) = ('344511', '02_Witchez-Chip_Yami_Richie')
+(TRN, ZOM, MAP_ONLY) = (103, 15, True)
+# Create output folder --------------------------------------------------------
+URL = 'https://awbw.amarriner.com/2030.php?games_id={}&ndx='.format(ID)
+OUT_PTH = path.join(cst.OUT_PTH, NAME)
+if not path.exists(OUT_PTH):
+    os.makedirs(OUT_PTH)
 # Load driver and mainpage ----------------------------------------------------
 print('* Loading selenium scraper...', end='\r')
 options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-driver = webdriver.Chrome(DRV, options=options)
+if cst.HEADLESS:
+    options.add_argument("--window-size=1920x1080")
+    options.add_argument("--headless")
+else:
+    options.add_argument("--start-maximized")
+driver = webdriver.Chrome(cst.DRV_PTH, options=options)
 driver.get('{}{}'.format(URL, 0))
 # Set zoom --------------------------------------------------------------------
-for i in range(8):
+for i in range(ZOM):
     driver.find_element_by_id('zoom-in').click()
+# Prepend identifier to filenames ---------------------------------------------
+prep = 'S'
+if MAP_ONLY:
+    prep = 'M'
 # Iterate through frames ------------------------------------------------------
-for ndx in range(0, TRN):
+for ndx in range(0, TRN+1):
     print('* Parsing ({}/{})'.format(ndx, TRN), end='\r')
     driver.get('{}{}'.format(URL, ndx))
-    # driver.set_window_size(1366, 728)
-    sleep(SLP)
-    # driver.execute_script("document.body.style.zoom='150%'")
-    imgName = 'turn_{}.png'.format(str(ndx).zfill(3))
-    imgPath = path.join(OUT, imgName)
-    element = driver.find_element_by_id('gamecontainer').screenshot(imgPath)
-sleep(SLP)
+    sleep(cst.SLEEP)
+    if MAP_ONLY:
+        element = driver.find_element_by_id('gamemap-container')
+    else:
+        element = driver.find_element_by_id('gamecontainer')
+    imgName = '{}_turn_{}.png'.format(prep, str(ndx).zfill(3))
+    imgPath = path.join(OUT_PTH, imgName)
+    element.screenshot(imgPath)
+# Close driver ----------------------------------------------------------------
+sleep(cst.SLEEP)
 print('* Done ({}/{})'.format(ndx, TRN), end='\r')
 driver.close()
 
-# # element = driver.find_element_by_id('gamemap-container')
-# (location, size) = (element.location, element.size)
-# driver.save_screenshot(path.join(OUT, imgName))
-# # crop image
-# (x, y) = (location['x'], location['y'])
-# width = location['x']+size['width']
-# height = location['y']+size['height']
-# im = Image.open(path.join(OUT, "screenshot.png"))
-# im = im.crop((int(x), int(y), int(width), int(height)))
-# im.save(path.join(OUT, imgName))
-
+# convert -delay 60 -resize 500x500 -loop 0 *.png animation.gif
+# convert -delay 60 -resize 500x500 -loop 0 *.png animation.gif
+# ffmpeg -f image2 -framerate 1 -i turn_%3d.png  out.gif
