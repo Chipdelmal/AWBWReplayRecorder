944f1acd855a7e828c2cc6dd163a70681aabf163
chipdelmal <chipdelmal@gmail.com>
2022-01-14 09:23:01 -0800

    Update to games lists and minor res

--- awbw.sh
+++ awbw.sh
@@ -16,17 +16,20 @@ NAME=$2
 TURNS=$3
 ZOOM=$4
 PTH=$5
-RES='300'
-DELAY='180'
+RES='290'
+DELAY='120'
 # Create paths and names for the frames and gifs -----------------------------
 IN="$PTH/$NAME/*.png"
 OUT_SM="$PTH/$NAME/${NAME}_small"
 OUT_LG="$PTH/$NAME/${NAME}"
+OUT_MD="$PTH/$NAME/${NAME}_medium"
 # Print filename to terminal -------------------------------------------------
 echo "${OUT_LG}.gif"
 # Scrape battle frames -------------------------------------------------------
 python main.py $ID $NAME $TURNS $ZOOM $PTH
-# Convert frames to gifs and mp4 ---------------------------------------------
+# # Convert frames to gifs and mp4 ---------------------------------------------
 convert -delay $DELAY -resize "${RES}x${RES}" -loop 0 $IN "${OUT_SM}.gif"
 convert -delay $DELAY -loop 1 $IN "${OUT_LG}.gif"
 convert -delay $DELAY -loop 1 $IN "${OUT_LG}.mp4"
+# Compres gifs ---------------------------------------------------------------
+gifsicle -i "${OUT_LG}.gif" -O3 --colors 256 -o "${OUT_MD}.gif"
--- batch.sh
+++ batch.sh
@@ -7,6 +7,34 @@ OUT='/home/chipdelmal/Documents/AWBW/'
 # ./awbw.sh "357091" "04_Booyah-Chip_Yami_Richie" "57" "8" $OUT
 # ./awbw.sh "348502" "05_WaifusYHusbandos-Chip_Yami_Richie_Memo" "100" "10" $OUT
 # ./awbw.sh "355237" "06_Water-Chip_Tomas" "46" "15" $OUT
-./awbw.sh "365401" "07_Aluxes-Chip_Yami_Richie" "82" "12" $OUT
-
-
+# ./awbw.sh "365401" "07_Aluxes-Chip_Yami_Richie" "82" "12" $OUT
+# ./awbw.sh "374691" "08_City-Chip_Tomas" "40" "12" $OUT
+# ./awbw.sh "369091" "09_Teams-Chip_Yami_Richie_Memo" "65" "12" $OUT
+# ./awbw.sh "346834" "10_FFAComrades-Chip_Yami_Tomas_Richie" "169" "15" $OUT
+# ./awbw.sh "381839" "11_LargeMap-Chip_Tomas" "38" "10" $OUT
+# ./awbw.sh "394177" "12_STP-Chip_Tomas" "31" "14" $OUT
+# ./awbw.sh "373429" "13_BilateralRelations-Yami_Tomas_Richie_Memo" "96" "8" $OUT
+# ./awbw.sh "380124" "14_Awita-Chip_Yami_Richie" "93" "15" $OUT
+# ./awbw.sh "402889" "15_Carnales-Chip_Danton" "31" "8" $OUT
+# ./awbw.sh "385040" "16_PenguinWars-Chip_Yami_Richie_Tomas" "94" "14" $OUT
+# ./awbw.sh "393618" "17_TiraFanboy-Chip_Yami_Richie_Christian" "60" "14" $OUT
+# ./awbw.sh "415740" "18_SinFanboy-Chip_Yami_Richie" "77" "12" $OUT
+# ./awbw.sh "410992" "19_Waifusbandos-Chip_Yami_Richie_Danton" "126" "10" $OUT
+# ./awbw.sh "430458" "20_Moaaar-Chip_Yami_Danton_Tomas" "104" "15" $OUT
+# ./awbw.sh "446073" "21_OraPrro-Chip_Danton" "40" "10" $OUT
+# ./awbw.sh "451279" "22_GrayDog-Chip_Danton" "39" "12" $OUT
+# ./awbw.sh "456776" "23_Foggy-Chip_Danton" "44" "18" $OUT
+# ./awbw.sh "424303" "24_Real3v3" "162" "15" $OUT
+# ./awbw.sh "441557" "25_GuerraGuerraSinTregua" "84" "15" $OUT
+# ./awbw.sh "460911" "26_USSR-Chip_Danton" "46" "15" $OUT
+# ./awbw.sh "468995" "27_BichotoEspacial-Chip_Danton" "27" "8" $OUT
+# ./awbw.sh "473358" "28_Android-Chip_Danton" "43" "15" $OUT
+# ./awbw.sh "453345" "29_PingusConMalaria-Chip_Yami_Richie" "70" "15" $OUT
+# ./awbw.sh "457895" "30_GLSTD-Chip_Nienna" "31" "15" $OUT
+# ./awbw.sh "478212" "31_SufragioEfectivo-Chip_Danton" "41" "15" $OUT
+# ./awbw.sh "435328" "32_Star-Chip_Yami_Danton_Christian_Richie" "148" "5" $OUT
+# ./awbw.sh "460918" "33_Repechaje-Chip_Yami_Danton_Richie" "102" "16" $OUT
+# ./awbw.sh "488362" "34_JFK-Chip_Danton_Chris_Richie" "76" "20" $OUT
+# ./awbw.sh "465486" "35_Standoff-Chip_Tomas_Danton_Yami_Richie_Memo" "181" "6" $OUT
+# ./awbw.sh "470866" "36_Emprendimiento-Chip_Yami_Richie_Memo_Danton_Christian" "125" "12" $OUT
+./awbw.sh "532999" "37_AdminWarsByWeb-Yami_Richie" "36" "12" $OUT
